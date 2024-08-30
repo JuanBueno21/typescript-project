971470a7100bda45aab934e01c642e48ec409385
JuanBueno21 <juancamilobuenomintic@gmail.com>
2024-08-30 10:33:12 -0500

    objeto prop y children

--- typescript/src/app/page.tsx
+++ typescript/src/app/page.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import {RandomFox} from "./components/RandomFox";
 
+const random = () => Math.floor(Math.random() * 123) + 1;
+
 export default function Home() {
   return (
     <main>
       <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
-    <RandomFox />
+    <RandomFox image={`https://randomfox.ca/images/${random()}.jpg`} />
     </main>
   );
 }
