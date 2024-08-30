779937fb9c627879b18e1d919a3598d2a0682819
JuanBueno21 <juancamilobuenomintic@gmail.com>
2024-08-30 11:38:13 -0500

    State with custom types

--- typescript/src/app/page.tsx
+++ typescript/src/app/page.tsx
@@ -4,20 +4,34 @@ import { useState } from "react";
 
 const random = () => Math.floor(Math.random() * 123) + 1;
 
+const generateId = () => Math.random().toString(36).substr(2, 9);
+
+type ImageItems = { id: string, url: string };
+
 export default function Home() {
-  const [images, setImages] = useState<Array<string>>([
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
+
+  //TIPOS PRIMITIVOS:
+  // const [images, setImages] = useState<Array<string>>([
+  //   `https://randomfox.ca/images/${random()}.jpg`,
+  //   `https://randomfox.ca/images/${random()}.jpg`,
+  //   `https://randomfox.ca/images/${random()}.jpg`,
+  //   `https://randomfox.ca/images/${random()}.jpg`,
+  // ]);
+
+  //TIPOS PERSONALIZADOS:
+  const [images, setImages] = useState<Array<ImageItems>>([
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
   ]);
 
   return (
     <main>
       <h1 className="text-3xl font-bold underline"> HOLA JUAN</h1>
-      {images.map((image, index) => (
-        <div key={index} className="p-4">
-          <RandomFox image={image} />
+      {images.map(({ id, url }) => (
+        <div key={id} className="p-4">
+          <RandomFox image={url} />
         </div>
       ))}
     </main>
