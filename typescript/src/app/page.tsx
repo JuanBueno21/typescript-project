9b3131f0b62e7a774fd2a58d76af966263384436
JuanBueno21 <juancamilobuenomintic@gmail.com>
2024-08-30 09:48:08 -0500

    define a component

---
+++ typescript/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import {RandomFox} from "./components/RandomFox";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <RandomFox />
+    </main>
+  );
+}
